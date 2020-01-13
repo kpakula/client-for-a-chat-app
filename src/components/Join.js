c051cb92a1f498ed320dce9955761bc8d5216abd
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-13 09:13:39 +0100

    Added basic view to login

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}></input></div>
+                <div><input placeholder="" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}></input></div>
+
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                <button className="button mt-20" type="submit">Sign in</button>
+                </Link>
+            </div>
+
+        </div>
     )
 }
 
