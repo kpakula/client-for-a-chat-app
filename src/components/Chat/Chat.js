1be9322e483e3edf263eb15ce2f41aaa4791b4d7
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-13 10:03:00 +0100

    Created a error handle callback

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -18,7 +18,9 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit("join", { name, room });
+        socket.emit("join", { name, room }, ({ error }) => {
+            alert(error);
+        });
 
     }, [ENDPOINT, location.search]);
 
