5c8f65efd08212790ee562deef8994326eda1615
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-17 00:17:52 +0100

    Updated view by responsive design

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 
 import "./Join.css";
 
-const Join = () => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    return (
-        <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
-                <div><input placeholder="" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}></input></div>
-                <div><input placeholder="" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}></input></div>
+const Join = ({ children }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                <button className="button mt-20" type="submit">Sign in</button>
-                </Link>
-            </div>
 
+  return (
+    <div className="outer">
+      <div className="inner">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="name"
+            className="inputField"
+            type="text"
+            onChange={event => setName(event.target.value)}
+          />
         </div>
-    )
-}
+        <div>
+          <input
+            placeholder="room"
+            className="inputField mt-20"
+            type="text"
+            onChange={event => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={event => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="joinBtn mt-20" type="submit">Sign in</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Join;
